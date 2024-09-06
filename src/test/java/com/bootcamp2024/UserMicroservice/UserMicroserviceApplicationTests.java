bec3c51dd80a6088cc354d14b59867f531457789
angel andres miranda castillo <mirandaangel83@gmail.com>
2024-09-05 19:34:19 -0500

    initial Spring Boot project setup

---
+++ src/test/java/com/bootcamp2024/UserMicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp2024.UserMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
